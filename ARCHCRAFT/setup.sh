4108884c633f022ebf12d3f1f97d993d94bb8b0d
NANACHANUWU <duongquocdung11042003@gmail.com>
2025-01-28 22:24:57 +0700

    ngon

--- ARCHCRAFT/setup.sh
+++ ARCHCRAFT/setup.sh
@@ -4,7 +4,7 @@
 sudo pacman -Syu --noconfirm
 
 # Cài đặt các ứng dụng từ official repositories
-sudo pacman -S --noconfirm arduino discord docker ffmpeg gimp git go gparted kubectl texlive-most minikube nmap wireshark-qt nodejs scrcpy steam unrar virtualbox vlc code wine
+sudo pacman -S --noconfirm arduino discord docker ffmpeg gimp git go gparted kubectl texlive-most minikube nmap wireshark-qt nodejs scrcpy steam unrar virtualbox vlc code wine 
 
 # Cài đặt các ứng dụng từ AUR sử dụng yay
 yay -S --noconfirm anaconda google-chrome insomnia lens-bin miniconda3 oh-my-posh-bin protonvpn pycharm-community-edition terabox
