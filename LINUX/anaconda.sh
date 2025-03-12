152ed4522047bae34cd0699b14bc624c4d9340e3
NANACHANUWU <duongquocdung11042003@gmail.com>
2025-03-12 15:46:44 +0700

    add last

--- LINUX/anaconda.sh
+++ LINUX/anaconda.sh
@@ -1,7 +1,6 @@
 sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 
 #!/bin/bash
-sudo apt update
 # Update package list
 
 # Install dependencies
--- LINUX/git.sh
+++ LINUX/git.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Update package list
-sudo apt update
 
 # Install Git
 sudo apt install -y git
